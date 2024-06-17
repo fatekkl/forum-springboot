d14eefad1f4c8c1c021ff4d45ff166289db31d9c
fatekkl <mathtml.1105@gmail.com>
2024-06-17 17:39:45 -0300

    feat: create id search

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -5,6 +5,7 @@ import br.com.alura.forum.model.Topico
 import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,5 +17,13 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping
     fun listar(): List<Topico> {
         return service.listar()
+    }
+
+
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): Topico{
+
+
+        return service.buscarPorId(id)
     }
 }
--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -7,16 +7,30 @@ import org.springframework.stereotype.Service
 
 
 @Service
-class TopicoService {
+class TopicoService(private var topicos: List<Topico>) {
 
+    init {
+        val curso = Curso(1,"teste", "teste")
+        val user = Usuario(1, "temten", "aaaa")
+
+        val topico = Topico(1, "teste", "teste1", curso = curso, autor =  user)
+        val topico2 = Topico(2,"teste2", "teste2", curso = curso, autor = user )
+
+        topicos = listOf(topico, topico2)
+    }
 
     fun listar(): List<Topico> {
 
-        val curso = Curso(1,"teste", "teste")
-        val user = Usuario(1, "temten", "aaaa")
-        val topico = Topico(1, "teste", "teste2", curso = curso, autor =  user)
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
 
 
-        return listOf(topico)
+        return topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+
+
     }
 }
