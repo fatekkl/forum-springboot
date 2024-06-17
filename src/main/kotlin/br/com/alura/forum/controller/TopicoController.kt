0b7f6ae45414c96fe98a344ca99caba211d59824
fatekkl <mathtml.1105@gmail.com>
2024-06-17 17:17:27 -0300

    feat: inject dependencies

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,13 +1,20 @@
 package br.com.alura.forum.controller
 
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import br.com.alura.forum.service.TopicoService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping ("/topicos")
-class TopicoController {
+class TopicoController(private val service: TopicoService) {
 
-    fun listar() {
 
+    @GetMapping
+    fun listar(): List<Topico> {
+        return service.listar()
     }
 }
