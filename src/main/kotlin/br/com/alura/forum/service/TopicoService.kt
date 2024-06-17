7b23e5af6a5c3dbcdbadcfe5a3db4b11db97f9f4
fatekkl <mathtml.1105@gmail.com>
2024-06-17 17:16:47 -0300

    feat: create GetAll() function

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,4 +1,22 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.stereotype.Service
+
+
+@Service
 class TopicoService {
+
+
+    fun listar(): List<Topico> {
+
+        val curso = Curso(1,"teste", "teste")
+        val user = Usuario(1, "temten", "aaaa")
+        val topico = Topico(1, "teste", "teste2", curso = curso, autor =  user)
+
+
+        return listOf(topico)
+    }
 }
