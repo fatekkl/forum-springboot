a99eb0ce9d94fc81614097ac141f1ec75bfd23f8
fatekkl <mathtml.1105@gmail.com>
2024-06-19 09:44:01 -0300

    feat: create DELETE method

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -58,4 +58,12 @@ class TopicoService(
 
         ))
     }
+
+    fun deletar(id: Long) {
+        val topico = topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+
+        topicos = topicos.minus(topico)
+    }
 }
