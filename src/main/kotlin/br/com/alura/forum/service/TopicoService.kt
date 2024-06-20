d67c914af1d7b3d163a1556c140bfb8c789901f0
fatekkl <mathtml.1105@gmail.com>
2024-06-20 10:31:18 -0300

    feat: handle HTTP 400

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -3,6 +3,7 @@ package br.com.alura.forum.service
 import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.exception.NotFoundException
 import br.com.alura.forum.mapper.TopicoFormMapper
 import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Topico
@@ -14,7 +15,8 @@ import org.springframework.stereotype.Service
 class TopicoService(
     private var topicos: List<Topico> = ArrayList(),
     private val topicoViewMapper: TopicoViewMapper,
-    private val topicoFormMapper: TopicoFormMapper
+    private val topicoFormMapper: TopicoFormMapper,
+    private val notFoundMessage: String = "Tópico não encontrado"
 ) {
 
     fun listar(): List<TopicoView> {
@@ -27,7 +29,9 @@ class TopicoService(
 
         val topico = topicos.stream().filter { t ->
             t.id == id
-        }.findFirst().get()
+        }.findFirst().orElseThrow{
+            NotFoundException(notFoundMessage)
+        }
 
         return topicoViewMapper.map(topico)
     }
@@ -47,7 +51,9 @@ class TopicoService(
 
         val topico = topicos.stream().filter { t ->
             t.id == novoTopico.id
-        }.findFirst().get()
+        }.findFirst().orElseThrow{
+            NotFoundException(notFoundMessage)
+        }
 
         val topicoAtualizado = Topico(id = novoTopico.id, titulo = novoTopico.titulo, mensagem = novoTopico.mensagem, curso = topico.curso, autor = topico.autor, respostas = topico.respostas, status = topico.status, dataCriacao = topico.dataCriacao)
 
@@ -59,7 +65,9 @@ class TopicoService(
     fun deletar(id: Long) {
         val topico = topicos.stream().filter { t ->
             t.id == id
-        }.findFirst().get()
+        }.findFirst().orElseThrow{
+            NotFoundException(notFoundMessage)
+        }
 
         topicos = topicos.minus(topico)
     }
